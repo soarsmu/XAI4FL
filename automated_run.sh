c7764fc5a32ad4ee8be10f1ff71828e044e09610
unknown <ratnadiraw@gmail.com>
2021-07-12 22:07:20 +0800

    [feat] Script to automate agnostic_model.py

---
+++ automated_run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+my_function () {
+	
+	mkdir -p "${project_name[$dir1]}/${project_name[$dir1]}-${x}"
+	
+}
+
+second_function () {
+    python agnositc_model_testing.py "${pwd}/${x}/spectra" "${pwd}/${x}/matrix" "${pwd}/${project_name[$dir1]}/${project_name[$dir1]}-${x}/${project_name[$dir1]}-${x}"
+	pwd
+
+}
+project_name=("Math")
+project_number=(103)
+x=80
+pwd=$(pwd)
+for dir1 in "${!project_name[@]}"; do
+  mkdir -p "\${project_name[$dir1]}"
+  echo "$dir1"
+  echo "${project_name[$dir1]}"
+  echo "${project_number[$dir1]}"
+  while [ $x -le ${project_number[$dir1]} ]
+  do
+     my_function
+     second_function
+     x=$(( $x + 1 ))
+  done
+done
+
