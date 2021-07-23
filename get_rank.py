d0fd29b441466a2c710ed208cceb240870c8dabb
unknown <ratnadiraw@gmail.com>
2021-07-23 12:51:54 +0800

    [feat] Check the line ans statements from the source code lines

--- get_rank.py
+++ get_rank.py
@@ -39,11 +39,18 @@ def get_results():
 	return all_rank
 
 def process_line(line):
+	#print(line)
 	split_line = line.split('#')
 	line_number = split_line[1]
 	split_line_1 = split_line[0].split('.')
 	file_name_wo_line = split_line_1[0].replace('/','.')
 	return file_name_wo_line +'#'+ line_number
+
+def back_line(line):
+	split_line = line.split('#')
+	line_number = split_line[1]
+	file_name_wo_line = split_line[0].replace('.','/')
+	return file_name_wo_line +'.java#'+ line_number
 
 
 def get_buggy_line(all_rank):
@@ -87,6 +94,48 @@ def get_sloc():
 def new_rank(data):
 	return rankdata([float(-1) * float(data[x]) for x in range(len(data))], method='average')
 
+def get_line(proj, bug, buggy_l):
+	# print(proj)
+	# print(bug)
+	# print(buggy_l)
+	buggy_line = back_line(buggy_l)
+	# print(buggy_line)
+	file1 = open("source-code-lines/"+proj+"-"+bug+"b.source-code.lines","r")
+	count = 0
+	get_from = ""
+	temp_1 = []
+	temp_2 = []
+	while True:
+	 
+	    # Get next line from file
+		line = file1.readline()
+	 
+	    # if line is empty
+	    # end of file is reached
+		if not line:
+			break
+		temp_arr = line.strip().split(":")
+		temp_1.append(temp_arr[0])
+		temp_2.append(temp_arr[1])
+		if temp_arr[1] == buggy_line:
+			get_from = temp_arr[0]
+			print(proj)
+			print(bug)
+			print("FOUUUUUNNDDDDD")
+			print(get_from)
+		count += 1
+	file1.close()
+
+	while get_from in temp_2:
+		temp_check = temp_2.index(get_from)
+		get_from = temp_1[temp_check]
+	if get_from == "":
+		return buggy_l
+	temp_line = process_line(get_from)
+	print(temp_line)
+	return temp_line
+
+
 def get_rank(all_rank,buggy_line,sloc_data):
 	ranked_final = buggy_line.copy()
 	temp_all_rank = all_rank.copy()
@@ -119,8 +168,8 @@ def get_rank(all_rank,buggy_line,sloc_data):
 							stmts_mean = stmts
 						else:
 							temp_stmts = stmts.split('#')
-							print(temp_stmts[0])
-							print(temp_stmts[1])
+							#print(temp_stmts[0])
+							#rint(temp_stmts[1])
 							for loop_in in temp_all_rank[project][bug_number]['max'][0]:
 								# print(loop_in)
 								if temp_stmts[0] in loop_in and temp_stmts[1] in loop_in:
@@ -130,6 +179,28 @@ def get_rank(all_rank,buggy_line,sloc_data):
 									stmts_max = loop_in
 									stmts_min = loop_in
 									stmts_mean = loop_in
+						if stmts_max == '':
+							new_stmts = get_line(project, bug_number, stmts)
+							if new_stmts in all_rank[project][bug_number]['max'][0]:
+								minimal_rank_max = sorted_max[all_rank[project][bug_number]['max'][0].index(new_stmts)]
+								minimal_rank_min = sorted_max[all_rank[project][bug_number]['min'][0].index(new_stmts)]
+								minimal_rank_mean = sorted_max[all_rank[project][bug_number]['mean'][0].index(new_stmts)]
+								stmts_max = new_stmts
+								stmts_min = new_stmts
+								stmts_mean = new_stmts
+							else:
+								temp_stmts = new_stmts.split('#')
+								#print(temp_stmts[0])
+								#print(temp_stmts[1])
+								for loop_in in temp_all_rank[project][bug_number]['max'][0]:
+									# print(loop_in)
+									if temp_stmts[0] in loop_in and temp_stmts[1] in loop_in:
+										minimal_rank_max = sorted_max[all_rank[project][bug_number]['max'][0].index(loop_in)]
+										minimal_rank_min = sorted_max[all_rank[project][bug_number]['min'][0].index(loop_in)]
+										minimal_rank_mean = sorted_max[all_rank[project][bug_number]['mean'][0].index(loop_in)]
+										stmts_max = loop_in
+										stmts_min = loop_in
+										stmts_mean = loop_in
 
 						for candidate in buggy_line[project][bug_number][stmts]:
 							if candidate in all_rank[project][bug_number]['max'][0]:
@@ -147,11 +218,13 @@ def get_rank(all_rank,buggy_line,sloc_data):
 									stmts_mean = candidate
 							else:
 								temp_stmts = candidate.split('#')
+								not_found_1 = True
 								#print(temp_stmts[0])
 								#print(temp_stmts[1])
 								for loop_in in temp_all_rank[project][bug_number]['max'][0]:
 									# print(loop_in)
 									if temp_stmts[0] in loop_in and temp_stmts[1] in loop_in:
+										not_found_1 = False
 										#print("FOUNDDDDD")
 										temp_max = sorted_max[all_rank[project][bug_number]['max'][0].index(loop_in)]
 										temp_min = sorted_max[all_rank[project][bug_number]['min'][0].index(loop_in)]
@@ -166,6 +239,42 @@ def get_rank(all_rank,buggy_line,sloc_data):
 											minimal_rank_mean = temp_mean
 											stmts_mean = loop_in
 										break
+								if not not_found_1:
+									new_candidate = get_line(project, bug_number, candidate)
+									if new_candidate in all_rank[project][bug_number]['max'][0]:
+										temp_max = sorted_max[all_rank[project][bug_number]['max'][0].index(new_candidate)]
+										temp_min = sorted_max[all_rank[project][bug_number]['min'][0].index(new_candidate)]
+										temp_mean = sorted_max[all_rank[project][bug_number]['mean'][0].index(new_candidate)]
+										if temp_max < minimal_rank_max:
+											minimal_rank_max = temp_max
+											stmts_max = new_candidate
+										if temp_min < minimal_rank_min:
+											minimal_rank_min = temp_min
+											stmts_min = new_candidate
+										if temp_mean < minimal_rank_mean:
+											minimal_rank_mean = temp_mean
+											stmts_mean = new_candidate
+									else:
+										temp_stmts = new_candidate.split('#')
+										#print(temp_stmts[0])
+										#print(temp_stmts[1])
+										for loop_in in temp_all_rank[project][bug_number]['max'][0]:
+											# print(loop_in)
+											if temp_stmts[0] in loop_in and temp_stmts[1] in loop_in:
+												#print("FOUNDDDDD")
+												temp_max = sorted_max[all_rank[project][bug_number]['max'][0].index(loop_in)]
+												temp_min = sorted_max[all_rank[project][bug_number]['min'][0].index(loop_in)]
+												temp_mean = sorted_max[all_rank[project][bug_number]['mean'][0].index(loop_in)]
+												if temp_max < minimal_rank_max:
+													minimal_rank_max = temp_max
+													stmts_max = loop_in
+												if temp_min < minimal_rank_min:
+													minimal_rank_min = temp_min
+													stmts_min = loop_in
+												if temp_mean < minimal_rank_mean:
+													minimal_rank_mean = temp_mean
+													stmts_mean = loop_in
+												break
 						ranked_final[project][bug_number][stmts] = {}
 						ranked_final[project][bug_number][stmts]['max'] = minimal_rank_max
 						ranked_final[project][bug_number][stmts]['min'] = minimal_rank_min
@@ -206,10 +315,39 @@ def get_rank(all_rank,buggy_line,sloc_data):
 									not_found_temp = 0
 									break
 							if not_found_temp == 1:
-								ranked_final[project][bug_number][stmts]['max'] = len(sorted_max) + 1
-								ranked_final[project][bug_number][stmts]['min'] = len(sorted_max) + 1
-								ranked_final[project][bug_number][stmts]['mean'] = len(sorted_max) + 1
-								ranked_final[project][bug_number][stmts]['not_found'] = '1'
+								new_stmts = get_line(project, bug_number, stmts)
+								if new_stmts in all_rank[project][bug_number]['max'][0]:
+									print("FOUUUUUNNDDDDD MOREEEEE")
+									print(sorted_max[all_rank[project][bug_number]['max'][0].index(new_stmts)])
+									ranked_final[project][bug_number][stmts]['max'] = sorted_max[all_rank[project][bug_number]['max'][0].index(new_stmts)]
+									ranked_final[project][bug_number][stmts]['min'] = sorted_max[all_rank[project][bug_number]['min'][0].index(new_stmts)]
+									ranked_final[project][bug_number][stmts]['mean'] = sorted_max[all_rank[project][bug_number]['mean'][0].index(new_stmts)]
+									ranked_final[project][bug_number][stmts]['not_found'] = '0'
+									print(ranked_final[project][bug_number][stmts]['max'])
+								else:
+									temp_stmts = new_stmts.split('#')
+									#print(temp_stmts[0])
+									#print(temp_stmts[1])
+									#print(temp_all_rank)
+									not_found_temp = 1
+									for loop_in in temp_all_rank[project][bug_number]['max'][0]:
+										# print(loop_in)
+										if temp_stmts[0] in loop_in and temp_stmts[1] in loop_in:
+											#print("FOUNDDDDD")
+											ranked_final[project][bug_number][stmts]['max'] = sorted_max[all_rank[project][bug_number]['max'][0].index(loop_in)]
+											ranked_final[project][bug_number][stmts]['min'] = sorted_max[all_rank[project][bug_number]['min'][0].index(loop_in)]
+											ranked_final[project][bug_number][stmts]['mean'] = sorted_max[all_rank[project][bug_number]['mean'][0].index(loop_in)]
+											ranked_final[project][bug_number][stmts]['max_stmts'] = loop_in
+											ranked_final[project][bug_number][stmts]['min_stmts'] = loop_in
+											ranked_final[project][bug_number][stmts]['mean_stmts'] = loop_in
+											ranked_final[project][bug_number][stmts]['not_found'] = '0'
+											not_found_temp = 0
+											break
+									if not_found_temp == 1:
+										ranked_final[project][bug_number][stmts]['max'] = len(sorted_max) + 1
+										ranked_final[project][bug_number][stmts]['min'] = len(sorted_max) + 1
+										ranked_final[project][bug_number][stmts]['mean'] = len(sorted_max) + 1
+										ranked_final[project][bug_number][stmts]['not_found'] = '1'
 						ranked_final[project][bug_number][stmts]['omission'] = '0'
 	return ranked_final
 	# print(ranked_final)
@@ -240,6 +378,7 @@ buggy_line_data = get_buggy_line(rank_data)
 sloc_data = get_sloc()
 #print(sloc_data)
 final_rank = get_rank(rank_data,buggy_line_data,sloc_data)
+print(final_rank['Chart']['13'])
 print_result(final_rank)
 
 
